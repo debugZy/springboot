1c6e65805dcc1ad3b6d4e5bb7e84cdd753eb4939
zydebug <zydebug@163.com>
2022-12-29 08:08:29 +0800

    第二次提交关于配置文件

--- springboot-starter/src/main/java/com/zy/Application.java
+++ springboot-starter/src/main/java/com/zy/Application.java
@@ -1,12 +1,22 @@
 package com.zy;
 
+import com.zy.config.AnotherConfiguration;
+import com.zy.config.SomeConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootConfiguration(proxyBeanMethods = false)
 @EnableAutoConfiguration
+@Import({SomeConfig.class, AnotherConfiguration.class})
 public class Application {
     @RequestMapping("/")
     String home() {
---
+++ springboot-starter/src/main/java/com/zy/service/impl/MyAccountService.java
@@ -0,0 +1,33 @@
+package com.zy.service.impl;
+
+import com.zy.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.PrintStream;
+
+/**
+ * If you structure your code as suggested above (locating your application class in a top package), you
+ * can add @ComponentScan without any arguments or use the @SpringBootApplication annotation which
+ * implicitly includes it. All of your application components (@Component, @Service, @Repository,
+ *
+ * @Controller, and others) are automatically registered as Spring Beans.
+ * If a bean has more than one constructor, you will need to mark the one you want Spring to use with
+ * @Autowired:
+ */
+
+@Service
+public class MyAccountService implements AccountService {
+    private String name;
+    private PrintStream out;
+
+    @Autowired
+    public MyAccountService(String name) {
+        this.name = name;
+    }
+
+    public MyAccountService(PrintStream out, String name) {
+        this.out = out;
+        this.name = name;
+    }
+}
