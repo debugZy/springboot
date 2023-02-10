46dba0060f82c1836cc15c9ee0bdcf89de7b0afe
honor01 <debugzy@163.com>
2023-02-10 10:54:58 +0800

    Java基础内部类基础

---
+++ javaTest/src/main/java/com/java/inter/Apply.java
@@ -0,0 +1,50 @@
+package com.java.inter;
+
+import java.util.Arrays;
+
+/**
+ * 将接口从具体实现中解耦使的接口可以应用于多种不同的具体实现，
+ */
+class Processor {
+    public String name() {
+        return getClass().getSimpleName();
+    }
+
+    Object process(Object input) {
+        return input;
+    }
+}
+
+class Upcase extends Processor {
+    String process(Object input) {
+        return ((String) input).toUpperCase();
+    }
+}
+
+class Downcase extends Processor {
+    String process(Object input) {
+        return ((String) input).toUpperCase();
+    }
+}
+
+class Splitter extends Processor {
+    String process(Object input) {
+        return Arrays.toString(((String) input).split(" "));
+    }
+}
+
+public class Apply {
+
+    public static void process(Processor p, Object s) {
+        System.out.println("Using Processor " + p.name());
+        System.out.println(p.process(s));
+    }
+
+    public static String s = "Disagreement with beliefs is by definition incorrect";
+
+    public static void main(String[] args) {
+        process(new Upcase(), s);
+        process(new Downcase(), s);
+        process(new Splitter(), s);
+    }
+}
