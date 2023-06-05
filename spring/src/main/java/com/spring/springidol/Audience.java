1c13f46d016345bc5ee909f4ff3df3c0fb761c09
honor01 <debugzy@163.com>
2023-06-05 16:23:38 +0800

    aop

---
+++ spring/src/main/java/com/spring/springidol/Audience.java
@@ -0,0 +1,53 @@
+package com.spring.springidol;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+
+@Aspect //切面
+public class Audience {
+    //定义切入点
+    @Pointcut("execution(* com.spring.springidol.Performer.perform(..))")
+    public void performance() {
+
+    }
+
+    @Before("performance()")
+    public void takeSeats() {
+        System.out.println("The audience is taking their seats.");
+    }
+
+    @Before("performance()")
+    public void turnOffCellPhones() {
+        System.out.println("The audience is turning off their cellphones");
+    }
+
+    @AfterReturning("performance()")
+    public void applaud() {
+        System.out.println("CLAP CLAP CLAP CLAP CLAP");
+    }
+
+    @AfterThrowing("performance()")
+    public void demadRefund() {
+        System.out.println("Boo! We want our money back!");
+    }
+
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis();
+            joinPoint.proceed();
+            long end = System.currentTimeMillis();
+            System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start) + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Audience().demadRefund();
+    }
+}
